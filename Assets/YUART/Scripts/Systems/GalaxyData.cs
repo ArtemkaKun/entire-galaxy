0dc1cf87cd8a968e1afbe684c859e51145b8177a
Artem Yurchenko <artem@studiovector.pl>
2020-11-18 22:41:25 +0100

    GalaxyData class was added; GalaxyManager class was added

---
+++ Assets/YUART/Scripts/Systems/GalaxyData.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Data class, that stores data of galaxy. 
+/// </summary>
+public sealed class GalaxyData
+{
+    public int mainStarsCount;
+    public int otherStarsCount;
+    
+    public int starsClassOCount;
+    public int starsClassBCount;
+    public int starsClassACount;
+    public int starsClassFCount;
+    public int starsClassGCount;
+    public int starsClassKCount;
+    public int starsClassMCount;
+    public int starsClassTCount;
+    public int starsClassWCount;
+    public int starsClassRCount;
+    public int starsClassNCount;
+    
+    public int planetsCount;
+    public int exoPlanetsCount;
+    public int satellitesCount;
+}
