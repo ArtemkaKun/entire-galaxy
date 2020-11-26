406d1ff18649f31b50d872f4500795d401b5c816
Artem Yurchenko <artem@studiovector.pl>
2020-11-26 19:25:25 +0100

    RangeChecker was added

---
+++ Assets/YUART/Scripts/Utilities/RangeChecker.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace YUART.Scripts.Utilities
+{
+    /// <summary>
+    /// Class, that provides methods to check if the value in range.
+    /// </summary>
+    public static class RangeChecker
+    {
+        public static bool CheckIfValueInRange(this float value, Vector2 range)
+        {
+            return value >= range.x && value <= range.y;
+        }
+        
+        public static bool CheckIfValueInRange(this float3 value, Vector2 range)
+        {
+            return value.x >= range.x && value.y >= range.x && value.z >= range.x
+                   && value.x <= range.y && value.y <= range.y && value.z <= range.y;
+        }
+    }
+}
