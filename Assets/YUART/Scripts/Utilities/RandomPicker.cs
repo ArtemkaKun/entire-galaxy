2e74de8b8c05cc623b8b2d9df535513d74b27538
Artem Yurchenko <artem@studiovector.pl>
2020-11-26 20:12:41 +0100

    RandomPicker class was added

---
+++ Assets/YUART/Scripts/Utilities/RandomPicker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YUART.Scripts.Utilities
+{
+    /// <summary>
+    /// Class, that provides methods for peaking random element from IEnumerable.
+    /// </summary>
+    public static class RandomPicker
+    {
+        public static T GetRandomElement<T>(this T[] array)
+        {
+           var randomElementIndex = new Random().Next(array.Length);
+
+           return array[randomElementIndex];
+        }
+    }
+}
