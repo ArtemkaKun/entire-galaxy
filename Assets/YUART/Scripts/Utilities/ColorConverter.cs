caf59948598a21dfcffbbc7714ac2177e9ca1e7e
Artem Yurchenko <artem@studiovector.pl>
2020-11-26 19:10:45 +0100

    ColorConverter tool was created

---
+++ Assets/YUART/Scripts/Utilities/ColorConverter.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace YUART.Scripts.Utilities
+{
+    /// <summary>
+    /// Class, that handles color conversion from Color32 to float4.
+    /// </summary>
+    public static class ColorConverter
+    {
+        public static float4 ConvertToFloat4(this Color32 color)
+        {
+            return new float4(color.a, color.b, color.g, color.r);
+        }
+    }
+}
