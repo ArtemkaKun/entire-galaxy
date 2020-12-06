30404b595a28a1ea78336ce9d117c6ea17049638
Artem Yurchenko <artem@studiovector.pl>
2020-12-06 22:49:10 +0100

    GalaxyManagerSingleton was added

---
+++ Assets/YUART/Scripts/Galaxy Manager/Systems/GalaxyManagerSingleton.cs
@@ -0,0 +1,12 @@
+﻿using YUART.Scripts.Utilities;
+
+namespace YUART.Scripts.Galaxy_Manager.Systems
+{
+    /// <summary>
+    /// Class, that represents GalaxyManager singleton.
+    /// </summary>
+    public sealed class GalaxyManagerSingleton : MonoSingleton<GalaxyManager>
+    {
+      
+    }
+}
---
+++ Assets/YUART/Scripts/Utilities/MonoSingleton.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace YUART.Scripts.Utilities
+{
+    /// <summary>
+    /// Base class for Mono singleton.
+    /// </summary>
+    public class MonoSingleton<T>
+    where T : MonoBehaviour
+    {
+        public static T Instance { get; } = GameObject.FindObjectsOfType<T>().FirstOrDefault();
+    }
+}
